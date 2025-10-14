defd094f52e77a26e54da1ff83c35110b9c8f520
4lV4R0 <alvaro@MacBook-Air-de-Alvaro.local>
2025-10-14 19:38:27 +0200

    Primer commit: Enalace de la calculadora en github

---
+++ app/src/main/java/dam/unidad/act3_ud3/MainActivity.java
@@ -0,0 +1,29 @@
+package dam.unidad.act3_ud3;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextNumber;
+    private double numero1 = 0;
+    private String operador = "";
+    private boolean nuevoNumero = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
